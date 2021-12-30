b990295f16e48d713b9a44e2a7c34d0aff12702c
Samuel-Lewis <mazuel@gmail.com>
2021-12-31 00:05:45 +1030

    Fix typo in modpack desc

--- src/panels/ModpackInfo.tsx
+++ src/panels/ModpackInfo.tsx
@@ -8,16 +8,24 @@ export const ModpackInfo: React.FC = () => {
     <div className="center panel panel-bordered">
       <P>Check out the official</P>
       <Title level={4}>
-        <A href="https://www.curseforge.com/minecraft/modpacks/iron-assembly-community-pack" rel="noopener noreferrer" target="_blank">
+        <A
+          href="https://www.curseforge.com/minecraft/modpacks/iron-assembly-community-pack"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           Iron Assembly Minecraft Modpack
         </A>
       </Title>
       <P>
         Designed to be a super <b>lightweight</b>, <b>performance</b> focussed,{" "}
-        <b>vanilla</b> experience. It's intention is to help you and your
-        friends get started in a 1.18 vanilla server as quick as possible.
+        <b>vanilla</b> experience. Its intention is to help you and your friends
+        get started in a 1.18 vanilla server as quickly as possible.
       </P>
-      <A href="https://www.curseforge.com/minecraft/modpacks/iron-assembly-community-pack" rel="noopener noreferrer" target="_blank">
+      <A
+        href="https://www.curseforge.com/minecraft/modpacks/iron-assembly-community-pack"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
         <img src="https://i.imgur.com/A8qQlIv.png" alt="" width={300} />
       </A>
     </div>
